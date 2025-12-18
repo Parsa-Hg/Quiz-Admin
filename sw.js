e7cabdeb0a243f5b89d1b66a53959da11871fec3
Parsa-Hg <parsahg277@gmail.com>
2025-12-18 19:30:49 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('azmik-v1').then((cache) => cache.addAll(['/index.html']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
