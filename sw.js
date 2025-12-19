bd81b209248ba614014540eecbc50ad2af98d089
Parsa-Hg <parsahg277@gmail.com>
2025-12-19 09:15:45 +0330

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'azmik-cache-v1';
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['./', './index.html', './icon.png', './manifest.json']);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
